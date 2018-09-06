b3512329859c2b55467954c32413a17b3cb2d02c
Jonnie Tyler <no reply>
2018-09-05 17:47:10 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 from flask import Flask, jsonify
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -20,32 +21,59 @@ app = Flask(__name__)
 def welcome():
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end>"
     )
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     timeframe=dt.date(2017,8,23) - dt.timedelta(days=365)
     
-    prcptimeframe = session.query(Measurement.prcp, Measurement.date).filter(Measurement.date>=timeframe).all()
+    prcptimeframe = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date>=timeframe).all()
 
     
-    precipitations = []
-    for record  in prcptimeframe:
-        precipitation_dict = {}
-        precipiation_dict["tobs"] = measurement.tobs
-        precipitation_dict["date"] = measurement.date
-        
-        precipitation.append(precipitation_dict)
+    precip = []
+    for tobs in prcptimeframe:
+        precip_dict = {}
+        precip_dict["date"] = tobs[1]
+        precip_dict["tobs"]=tobs[0]
+        precip.append(precip_dict)
 
-    return jsonify(precipitations)
+    return jsonify(precip)
 
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
 
+    stations = list(np.ravel(results))
 
+    return jsonify(stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    start_date=dt.date(2016,8,23) - dt.timedelta(days=365)
+    end_date = dt.date(2016,8,23)
+    
+    results = session.query(Measurement.tobs).filter(Measurement.date>=start_date).filter(Measurement.date<=end_date) .all()
+    tobs_results = list(np.ravel(results))
+
+    return jsonify(tobs_results)
+
+@app.route("/api/v1.0/<start>")
+def start():
+    start_date=dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    start_stats = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs).filter(Measurement.date>=start_date)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start():
+    start_date=dt.date(2017,1,20)
+    end_date = dt.date(2017,1,31)
+    
+
+    start_end_stats = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs).filter(Measurement.date>=start_date).filter(Measurement.date<=end_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
